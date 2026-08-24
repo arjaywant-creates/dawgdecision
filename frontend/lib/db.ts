fb8cab5336ab8d1b96ab532d72dde1db4e4df5a1
Shafil Alam <alamshafil@pm.me>
2026-08-23 20:29:20 -0400

    Remove debug logs from db.ts

--- frontend/lib/db.ts
+++ frontend/lib/db.ts
@@ -3,9 +3,6 @@ import { PrismaPg } from "@prisma/adapter-pg";
 
 import { PrismaClient } from "../generated/prisma/client";
 
-console.log("CWD:", process.cwd());
-console.log("DATABASE_URL:", process.env.DATABASE_URL);
-console.log("SHADOW_DATABASE_URL:", process.env.SHADOW_DATABASE_URL);
 const connectionString = `${process.env.DATABASE_URL}`;
 
 const adapter = new PrismaPg({ connectionString });
